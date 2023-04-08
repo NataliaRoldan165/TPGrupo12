4458636bcfb7dd067fae05d42ca005a07c3a033c
amoltoni <amoltoni@yahoo.com>
2023-04-08 11:35:59 -0300

    agregados para la seccion contacto

---
+++ app_Contacto.js
@@ -0,0 +1,16 @@
+const Contacto_Enviar = document.getElementById("btn-enviar");
+const Correo_Electronico_Ingresado = document.getElementById("email");
+const Nombre_Ingresado = document.getElementById("fullname");
+const Texto_Ingresado = document.getElementById("Comentario");
+
+
+Contacto_Enviar.addEventListener("click",Envio_de_Informacion_de_Contacto);
+
+
+function Envio_de_Informacion_de_Contacto()
+    {
+        //aca hay que escribir codigo que utilice la informacion cargada por el usuario (nombre, email, comentario) 
+        //hay que definirlo
+
+        console.log("se presionó el boton enviar");
+    }
